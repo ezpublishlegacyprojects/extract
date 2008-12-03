86516b435fe7291993146eea4c4655f085e5db4a
Björn Dieding <bjoern@xrow.de>
2008-12-03 16:24:10 +0000

    - bugfix and copyright changes
    git-svn-id: http://svn.projects.ez.no/extract@16 49c4204c-ea53-4b9d-8461-b199988ea03a

--- trunk/extension/extract/ezinfo.php
+++ trunk/extension/extract/ezinfo.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * File containing the adminInfo class.
+ * File containing the extractInfo class.
  *
  * @package extract
  * @version //autogentag//
- * @copyright Copyright (C) 2007 xrow. All rights reserved.
+ * @copyright Copyright (C) 2008 xrow. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl.txt GPL License
  */
-class ezadminInfo
+class extractInfo
 {
     function info()
     {
         return array(
             'Name' => "eZ Publish extract extension",
             'Version' => "1.0",
-            'Copyright' => "Copyright (C) 2007 xrow GbR",
-            'Author' => "xrow GbR",
+            'Copyright' => "Copyright (C) 2008 xrow GmbH",
+            'Author' => "xrow GmbH",
             'License' => "GPL License" );
     }
 }
