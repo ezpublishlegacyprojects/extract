5d25005ab35519e979e5fb443b028a0852587424
Björn Dieding <bjoern@xrow.de>
2010-06-01 12:27:49 +0000

    - fixing issues
    git-svn-id: http://svn.projects.ez.no/extract@19 49c4204c-ea53-4b9d-8461-b199988ea03a

--- trunk/extension/extract/modules/extract/module.php
+++ trunk/extension/extract/modules/extract/module.php
@@ -4,7 +4,11 @@ $Module = array('name' => 'Extract');
 
 $ViewList = array();
 $ViewList['csv'] = array( 'script' => 'csv.php',
+                          'functions' => array( 'csv' ),
             			  'default_navigation_part' => 'ezextractnavigationpart',
             			  'post_actions' => array( 'Download', 'BrowseSubtree', 'AddAttribute', 'Remove', 'RemoveData' ),
             			  'params' => array() );
+
+$FunctionList = array();
+$FunctionList['csv'] = array();
 ?>
